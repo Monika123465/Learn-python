b5ae26d22ec6a808286c9488889240c4e3e15dbc
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-09-03 21:12:05 +0530

    print python

--- print.py
+++ print.py
@@ -1,1 +1,28 @@
-print("hello world")
+#print("hello world")
+ 
+x=7
+if x>5:
+    print(" x is greater than 5")
+
+
+
+a="hello World !"
+print(a[4])
+
+
+n="awesome"
+def myfunc():
+    n="fantastic"
+    print("python is "+n)
+    
+myfunc()
+
+print("python is"+n)
+
+txt="The best things in life are free !"
+print("free" in txt)
+
+text="The best things in life are free !"
+
+if "expensive" not in text:
+    print("No","expansive is not present.")
