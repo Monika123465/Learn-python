12505b5d8b936a9c3d3355e9ce2cebb83fbb3ed5
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-09-12 22:47:06 +0530

    loop

--- loop.py
+++ loop.py
@@ -16,12 +16,84 @@
 # superhero=input("who is your superhero?:")
 # print(superhero)
 
-old_age=input("enter your old age:")
-new_age=int(old_age)+2        #add type function or define int/float/complex 
-print(new_age)
+# old_age=input("enter your old age:")
+# new_age=int(old_age)+2        #add type function or define int/float/complex 
+# print(new_age)
+
+# a=33
+# b=34
+# if b>a:
+#     print("b is greater than a")
+# elif a==b:
+#     print("a and b are equal")
+
+a=400
+b=240
+c=500
+# if b>a:
+#     print("b is greater tha a")
+# elif a==b:
+#     print("a is equal to b")
+# else:
+#     print("a is greater than b")
+# if a<b and c>a:
+#     print("both condition are true")
+# elif a<b or c<a:
+#     print ("only one condition are true")
+# else:
+#     print('true for anyother condition')
 
 
-          
+# if not a <b:
+#     print("a is NOT greater than b")
+
+# x=41
+# if x>10:
+#     print("above ten")
+# if(x>20):
+#     print("and also above 20!")
+# else:
+#     print("but not above 20")
+ 
+ #break
+# i=1
+# while i<6:
+#     print(i)
+#     if i==3:
+#         break
+#     i+=1
+
+#continue
+
+# i=1
+# while i<10:
+#     i+=1
+#     if i==3:
+#         continue
+    # print(i)
+
+    #for loops
+# fruits=['apple','banana','cherry']
+# for x in fruits:
+#     print(x)
+#     if x=="banana":
+#         break
+
+# for x in fruits:
+#     if x=="banana":
+#         continue
+#     print(x)
+
+for x in range(6):
+    print(x)
+
+for x in range(2,6):
+    print(x)
+
+for x in range(2,30,3):
+    print(x)
+
+
 
 
 
