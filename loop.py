68e846bd3e18090a5e24bfc6bb63d13b7dd3a257
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-09-13 23:14:32 +0530

    practice nested loop

--- loop.py
+++ loop.py
@@ -84,17 +84,36 @@ c=500
 #         continue
 #     print(x)
 
-for x in range(6):
-    print(x)
+# for x in range(6):
+#     print(x)
 
-for x in range(2,6):
-    print(x)
+# for x in range(2,6):
+#     print(x)
 
-for x in range(2,30,3):
-    print(x)
+# for x in range(2,30,3):
+#     print(x)
 
+# for x in range(6):
+#    print(x)
+# else:
+#    print("finally finished")
 
+# for x in range(8):
+#    if x==3 : break
+#    print(x)
+# else:
+#    print("finally finished")
 
+# # nested loop
+# adj=["red","big","tasty"]
+# fruits=["apple","banana","cherry"]
+
+# for x in adj:
+#     for y in fruits:
+#         print(x,y)
+
+# for x in [0,1,2]:
+#     pass
 
 
 
