3e8e7aba33259920bc0385d59806b8a3be3b2024
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-09-25 14:17:48 +0530

    claass added

---
+++ class.py
@@ -0,0 +1,34 @@
+#Swapping program
+
+# x=10
+# y=50
+
+# temp=x
+# x=y
+# y=temp
+# print("value of x:",x)
+# print("value of y:",y)
+
+
+# program to check if a no is prime or not 
+#input:23
+ 
+# num=int(input("enter a number:"))
+# flag=False
+
+# if num >1:
+#     for i in range(2,num):
+#         if(num%i)==0:
+#             flag=True
+#             break
+
+
+
+# if flag:
+#      print(num,"is not a prime number")
+# else:
+#      print(num,"is a prime no.")     
+
+     # program to check if a no is even or not
+
+
