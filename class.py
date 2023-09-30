21a5655d8d7ce322fba08332a6e4bc9593d86372
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-09-30 07:47:48 +0530

    add fiabonacci series

--- class.py
+++ class.py
@@ -22,13 +22,22 @@
 #             flag=True
 #             break
 
+# fibonacci series
+
+num=10
+n1,n2=0,1
+print("fibonacci Series:",n1,n2, end="")
+
+for i in range(2,num):
+    n3=n1+n2
+    n1=n2
+    n2=n3
+    print( n3,end="")
 
 
-# if flag:
-#      print(num,"is not a prime number")
-# else:
-#      print(num,"is a prime no.")     
 
-     # program to check if a no is even or not
+print()
+
+
 
 
