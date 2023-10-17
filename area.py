74555463f4a57a0279c19b3e5fa150ffa3403fd8
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-10-17 22:46:26 +0530

    calculate area done

---
+++ area.py
@@ -0,0 +1,11 @@
+
+def calculate_area(a,b):
+    
+    c=a*b
+    print("area of rectangle is :",c)
+    
+    
+a=int(input("Please enter the width:"))
+b=int(input("please enter the length:"))
+calculate_area(a,b)
+
