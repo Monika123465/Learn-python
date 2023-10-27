58d4a65eb220991a1955bc7a731c0a24f76dc837
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-10-27 22:26:41 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,10 @@
+mytuple=("apple","banana","cherry")
+print(mytuple)
+print(len(mytuple))
+
+thistuple=("apple",)
+print(type(thistuple))
+
+tuple1=("abc",34,True,40,'male')
+print(tuple1)
+print(mytuple[1])
