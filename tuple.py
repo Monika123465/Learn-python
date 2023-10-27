34edd622597964505d39bfd91b03fa873b28e838
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-10-27 23:32:49 +0530

    tuple

--- tuple.py
+++ tuple.py
@@ -1,4 +1,4 @@
-mytuple=("apple","banana","cherry")
+mytuple=("apple","banana","cherry","orange","kiwi","melon")
 print(mytuple)
 print(len(mytuple))
 
@@ -7,4 +7,15 @@ print(type(thistuple))
 
 tuple1=("abc",34,True,40,'male')
 print(tuple1)
-print(mytuple[1])
+print(mytuple[-1])
+print(mytuple[2:5])
+print(mytuple[:4])
+print(thistuple[2:])
+print(thistuple[-4:1])
+
+if "apple" in mytuple:
+    print("yes","apple is in the fruits tuple")
+
+x=("apple","banana","cherry")
+if "papaya" in thistuple:
+    print("yes","apple is in the fruits tuple") 
