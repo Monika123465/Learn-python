476d9ac1ea2d4e7a71e73d479c731384e0fe51d4
Monika kumari <105919665+Monika123465@users.noreply.github.com>
2023-11-06 21:22:13 +0530

    keyvalue

--- dist.py
+++ dist.py
@@ -8,3 +8,13 @@ print(thisdict)
 
 newdict=dict(name="john",age=36,country="norway")
 print(newdict)
+
+x=thisdict.keys()
+print(x)
+
+thisdict["brand"]="audi"
+print(thisdict)
+x=thisdict.values()
+print(x)
+thisdict["year"]=2000
+print(thisdict)
